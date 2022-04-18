260972d3da4dfb770f500571dd3ae353ef3fc1c7
nhachelaf99 <102677595+nhachelaf99@users.noreply.github.com>
2022-04-18 16:06:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-document.addEventListener("mousemove", parallax);
+const intro = document.getElementById("intro-container")
+intro.addEventListener("mousemove", parallax);
 function parallax(e) {
     document.querySelectorAll(".par-img").forEach(function(move){
         let movie_value = move.getAttribute("data-value")
         let x = (e.clientX * movie_value) / 150;
         let y = (e.clientY * movie_value) / 150;
 
-        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px) rotate("+ x +"deg)"
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)  rotate("+ x +"deg)"
 
 
     })
